a17a540368c547109595ff83cdd2de32b782f7d0
colbycookn <coocl-24@rhodes.edu>
2024-02-24 13:02:45 -0600

    questionsCreator now deals with Misc, History/Science Questions; questions_resource has been updated with questions

--- questionCreator.swift
+++ questionCreator.swift
@@ -13,6 +13,10 @@ var moviesSub = [String]()
 var musicSub = [String]()
 var tvSub = [String]()
 var sportsSub = [String]()
+var historySub = [String]()
+var scienceSub = [String]()
+var HoSsub = [String]()
+var miscSub = [String]()
 
 
 // Questions Lists
@@ -20,12 +24,20 @@ var moviesQuestions = [String]()
 var musicQuestions = [String]()
 var tvQuestions = [String]()
 var sportsQuestions = [String]()
+var historyQuestions = [String]()
+var scienceQuestions = [String]()
+var HoSQuestions = [String]()
+var miscQuestions = [String]()
 
 // Answers Lists
 var moviesAnswers = [String]()
 var musicAnswers = [String]()
 var tvAnswers = [String]()
 var sportsAnswers = [String]()
+var historyAnswers = [String]()
+var scienceAnswers = [String]()
+var HoSAnswers = [String]()
+var miscAnswers = [String]()
 
 // Read question text file
 // Create list of subcategory, question, and answer for every category
@@ -60,6 +72,22 @@ func readFile(filePath: String) {
                 sportsSub.append(subcategory)
                 sportsQuestions.append(question)
                 sportsAnswers.append(answer)
+            case "History":
+                historySub.append(subcategory)
+                historyQuestions.append(question)
+                historyAnswers.append(answer)
+            case "Science":
+                scienceSub.append(subcategory)
+                scienceQuestions.append(question)
+                scienceAnswers.append(answer)
+            case "HoS":
+                HoSsub.append(subcategory)
+                HoSQuestions.append(question)
+                HoSAnswers.append(answer)
+            case "Misc":
+                miscSub.append(subcategory)
+                miscQuestions.append(question)
+                miscAnswers.append(answer)
             default:
                 break
             }
@@ -78,8 +106,12 @@ func getTheme(day: String) -> String {
         return "Music"
     case "Thursday":
         return "TV"
-    default:
+    case "Saturday":
         return "Sports"
+    case "Sunday":
+        return "History/Science"
+    default:
+        return "Misc"
     }
 }
 
@@ -120,6 +152,38 @@ func getQuestions(theme: String) -> [Int: [String]] {
             sportsAnswers.removeFirst()
             sportsSub.removeFirst()
             i += 1
+        }
+    case "Misc":
+        while i < 11 {
+            questions[i] = [miscQuestions[0], miscAnswers[0], miscSub[0]]
+            miscQuestions.removeFirst()
+            miscAnswers.removeFirst()
+            miscSub.removeFirst()
+            i += 1
+        }
+    case "History/Science":
+        while i < 11 {
+            if i == 0 || i == 4 || i == 10 {
+                questions[i] = [HoSQuestions[i], HoSAnswers[i], HoSsub[i]]
+                HoSQuestions.remove(at : i)
+                HoSAnswers.remove(at: i)
+                HoSsub.remove(at: i)
+                i += 1
+            }
+            else if i == 1 || i == 3 || i == 6 || i == 7 {
+                questions[i] = [historyQuestions[i], historyAnswers[i], historySub[i]]
+                historyQuestions.remove(at : i)
+                historyAnswers.remove(at: i)
+                historySub.remove(at: i)
+                i += 1
+            }
+            else {
+                questions[i] = [scienceQuestions[i], scienceAnswers[i], scienceSub[i]]
+                scienceQuestions.remove(at : i)
+                scienceAnswers.remove(at: i)
+                scienceSub.remove(at: i)
+                i += 1
+            }
         }
     default:
         break
