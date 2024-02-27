6c819c245a39825b88cc08666f88cde6e54b47e3
colbycookn <coocl-24@rhodes.edu>
2024-02-27 12:53:11 -0600

    Deleted multiple view files and implemented their code to ContentView. Now have a simple, playable game.

--- ContentView.swift
+++ ContentView.swift
@@ -7,18 +7,53 @@
 // HUGO Testing
 
 import SwiftUI
+var theme = getTheme(day:"Saturday")
+var questions_dict = question_caller()
 
 
+//Replica Path used for assigning color
+var colorPath: [Int: UIColor] = [0: .gray,
+                                 1: .gray,
+                                 2: .gray,
+                                 3: .gray,
+                                 4: .gray,
+                                 5: .gray,
+                                 6: .gray,
+                                 7: .gray,
+                                 8: .gray,
+                                 9: .gray,
+                                10: .gray]
+
+// Create Path process
+var daily_path = [Int: [String]]()
+func create_path() -> [Int: [String]] {
+    for i in 0...10 {
+        if let value = questions_dict[i] {
+            daily_path[i] = value
+        }
+    }
+    questions_dict.removeAll()
+    return daily_path
+}
+
+
+
+//creates path with question_dict that has the 11 questions, answers, subcats ready for the day
+var path = create_path()
+
+
+
+var path_node: Int = 0
+var round_num: Int = 1
 struct ContentView: View{
     var body: some View {
             NavigationView {
                 VStack{
-                    Image("Logo")
                     HStack{
                         Text("TriviYeah!")
                             .font(.largeTitle)
                             .fontWeight(.heavy)
-                            .foregroundColor(Color.red)
+                            .foregroundColor(Color.purple)
                             .multilineTextAlignment(.center)
                     }
                     NavigationLink{
@@ -29,6 +64,313 @@ struct ContentView: View{
                 }
             }
         
+    }
+}
+
+
+
+//LinkView
+struct LinkView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+
+
+
+
+
+//Question Scene
+//Round Play
+struct QuestionView: View{
+    @State private var answer = ""
+    @State private var isCorrect: Bool?
+    var body: some View {
+        VStack {
+            
+            
+            if round_num == 1{
+                Text("Round: \(round_num)")
+                    .font(.title)
+                    .padding()
+                Text("Question: \(path[0]![0])")
+                    .padding()
+                TextField("Enter your answer", text: $answer)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Submit") {
+                    //Change logic so the right answer is checked and its not hard coded
+                    if answer == "\(path[0]![1])"{
+                        isCorrect = true}
+                    else{
+                        isCorrect = false}
+                    
+                    }
+                .padding()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .disabled(answer.isEmpty)
+                
+            }
+                else if round_num != 5 {
+                        Text("Round: \(round_num)")
+                            .font(.title)
+                            .padding()
+                        Text("Question: \(path[path_node]![0])")
+                            .padding()
+                        TextField("Enter your answer", text: $answer)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                        
+                        Button("Submit") {
+                            //Change logic so the right answer is checked and its not hard coded
+                            if answer == "\(path[path_node]![1])"{
+                                isCorrect = true}
+                            else{
+                                isCorrect = false}
+                            
+                        }
+                        .padding()
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/) //look into
+                        .disabled(answer.isEmpty)
+                    
+                }
+            else{
+                Text("FINAL ROUND")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.purple)
+                Text("Question: \(path[10]![0])")
+                    .padding()
+                TextField("Enter your answer", text: $answer)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Submit") {
+                    //Change logic so the right answer is checked and its not hard coded
+                    if answer == "\(path[10]![1])"{
+                        isCorrect = true}
+                    else{
+                        isCorrect = false}
+                    
+                }
+                .padding()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .disabled(answer.isEmpty)
+            
+        }
+            
+                
+            }
+            
+            
+        if round_num != 4 && round_num != 5{
+            if isCorrect == true {
+                //Tells user they are correct and moves to the sub cat selection screen
+                Text("Correct!")
+                    .foregroundColor(.green)
+                NavigationLink(destination: CategoryView()){
+
+                    PrimeButton_(text: "Continue")
+                }.navigationBarBackButtonHidden(true)
+                .onAppear {
+                        round_num += 1
+                    }
+                
+            }
+            else if isCorrect == false{
+                //Finishes the game and shows tree progress
+                Text("Incorrect!")
+                    .foregroundColor(.red)
+                NavigationLink(destination: GameOverView()){
+                    PrimeButton_(text: "Continue")
+                    
+                }.navigationBarBackButtonHidden(true)
+                }
+        }
+        else if round_num == 4{
+            if isCorrect == true {
+                //Tells user they are correct and moves to the sub cat selection screen
+                Text("Correct!")
+                    .foregroundColor(.green)
+                NavigationLink(destination: QuestionView()){
+
+                    PrimeButton_(text: "Continue")
+                }.navigationBarBackButtonHidden(true)
+                .onAppear {
+                        round_num += 1
+                    }
+                
+            }
+            else if isCorrect == false {
+                //Finishes the game and shows tree progress
+                Text("Incorrect!")
+                    .foregroundColor(.red)
+                NavigationLink(destination: GameOverView()){
+                    PrimeButton_(text: "Continue")
+                    
+                }.navigationBarBackButtonHidden(true)
+                }
+        } else{
+            if isCorrect == true {
+                //Tells user they are correct and moves to the sub cat selection screen
+                Text("Correct!")
+                    .foregroundColor(.green)
+                NavigationLink(destination: GameOverView()){
+
+                    PrimeButton_(text: "Continue")
+                }.navigationBarBackButtonHidden(true)
+                .onAppear {
+                        round_num += 1
+                    }
+                
+            }
+            else if isCorrect == false {
+                //Finishes the game and shows tree progress
+                Text("Incorrect!")
+                    .foregroundColor(.red)
+                NavigationLink(destination: GameOverView()){
+                    PrimeButton_(text: "Continue")
+                    
+                }.navigationBarBackButtonHidden(true)
+                }
+        }
+
+            }
+            }
+        
+
+
+
+struct CategoryView: View{
+    var body: some View {
+            NavigationView {
+                VStack{
+                    Text("Pick Next Round's Category!")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.purple)
+                        
+                    HStack{
+                        
+                        if round_num == 2{
+                            NavigationLink(destination: QuestionView()) {
+                                PrimeButton_(text: path[1]![2])
+                                
+                                
+                            }
+                            .simultaneousGesture(TapGesture().onEnded {
+                                path_node = 1
+                            })
+                            NavigationLink(destination: QuestionView()){
+                                PrimeButton_(text: path[2]![2])
+                            }
+                            .simultaneousGesture(TapGesture().onEnded {
+                                path_node = 2
+                            })
+                        }
+                        else if round_num == 3{
+                            if path_node == 1{
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[3]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 3
+                                })
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[4]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 4
+                                })
+                            }
+                            else{
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[4]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 4
+                                })
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[5]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 5
+                                })
+                            }
+                        }
+                        else{
+                            if path_node == 3{
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[6]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 6
+                                })
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[7]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 7
+                                })
+                            }
+                            else if path_node == 4{
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[7]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 7
+                                })
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[8]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 8
+                                })
+                            }
+                            else{
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[8]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 8
+                                })
+                                NavigationLink(destination: QuestionView()) {
+                                    PrimeButton_(text: path[9]![2])
+                                }.simultaneousGesture(TapGesture().onEnded {
+                                    path_node = 9
+                                })
+                            }
+                        }
+                        
+                    }
+                }
+            }.navigationBarHidden(true)
+        
+    }
+}
+
+
+
+
+
+//Game Over scene
+struct GameOverView: View{
+    var body: some View {
+        VStack{
+            Text("Game Over!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.purple)
+            
+           if round_num == 6{
+                Text("Congratulations, You got a TriviYeah!")
+                   .font(.largeTitle)
+                   .fontWeight(.heavy)
+                   .foregroundColor(Color.green)
+            }
+            else{
+                Text("Maybe next time Sport!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.red)
+            }
+            }
+            //Figure out a way to  present tree results
+            //Ideas(dumb) have an image of the tree for every possible combinations of right and wrong squares and insert the image based on player preformance
+            //Have a changeable "game object"?? that changes as the user plays
     }
 }
 
--- IOS/Components /PrimeButton .swift
+++ IOS/Components /PrimeButton .swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct PrimeButton_: View {
     var text: String
-    var background: Color = Color("AccentColor")
+    var background: Color = Color(.purple)
     
     var body: some View {
         Text(text)
-            .foregroundColor(.black)
+            .foregroundColor(.white)
             .padding()
             .padding(.horizontal)
             .background(background)
