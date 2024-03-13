a1cd77b3242daf5eb910d0ac8d0b7e3baf5f17fc
charliean0501 <charliean4@gmail.com>
2024-03-13 08:46:08 -0400

    c

--- IOS/Components /PrimeButton .swift
+++ IOS/Components /PrimeButton .swift
@@ -13,9 +13,9 @@ struct PrimeButton_: View {
 
     var body: some View {
         Text(text)
-            .font(Font.custom("Arial Rounded MT Bold", size: 30)) // Using custom font
+            .font(Font.custom("Arial Rounded MT Bold", size: 25)) // Using custom font
             .foregroundColor(.skyTeal)
-            .frame(width: 80, height: 28) // Set width and height
+            .frame(width: 120, height: 28) // Set width and height
             .padding()
             .background(background)
             .cornerRadius(0) // Remove corner radius
