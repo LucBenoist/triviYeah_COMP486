d08f1827c4d6f7b44be26264907e432e01dc2931
colbycookn <coocl-24@rhodes.edu>
2024-04-16 12:59:18 -0500

    MAde PathView

--- IOS/PathView.swift
+++ IOS/PathView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PathView: View {
     var body: some View {
         ZStack{
-            Color("Navy")
+            Color(Color.black)
                 .ignoresSafeArea()
             Rectangle()
                 .stroke(Color.hotPink, lineWidth: 4)
@@ -18,6 +18,23 @@ struct PathView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack{
+                Text("Here are two examples of Gameplay results. The trees are made of five rows, where each row represents a round. After you progress through each round you can choose between two subcategories. The first round and final round will have one question with no other choices.")
+                    .foregroundColor(.skyYellow)
+                    .multilineTextAlignment(.center)
+                HStack{
+                    VStack{
+                        Image("Path Results")
+                        Text("Here is the result of game where user failed in the third round.")
+                            .foregroundColor(.skyYellow)
+                            .multilineTextAlignment(.center)
+                    }
+                    VStack{
+                        Image("Perfect Path")
+                        Text("Here is the user got a perfect game, a TriviYeah!")
+                            .foregroundColor(.skyYellow)
+                            .multilineTextAlignment(.center)
+                    }
+                }
                 NavigationLink(destination: InstructionsView()) {
                     PrimeButton_(text: "Back")
                     
