a16b5a33ffd6a2bc43dac9a1689eae6c2c0d8eeb
colbycookn <coocl-24@rhodes.edu>
2024-04-23 20:21:10 -0500

    Text fits in prime button

--- IOS/Components /PrimeButton .swift
+++ IOS/Components /PrimeButton .swift
@@ -15,16 +15,16 @@ struct PrimeButton_: View {
 
     var body: some View {
         Text(text)
-            .font(Font.custom("Arial Rounded MT Bold", size: 22))
+            .font(text.count <= 10 ? Font.custom("Arial Rounded MT Bold", size: 26) : text.count <= 12 ? Font.custom("Arial Rounded MT Bold", size: 17) : text.count <= 16 ? Font.custom("Arial Rounded MT Bold", size : 16) : Font.custom("Arial Rounded MT Bold", size : 13))
             .foregroundColor(.skyYellow)
-            .frame(width: 135, height: 27)
+            .frame(width: 128, height: 27)
             .padding()
             .background(background)
             .cornerRadius(cornerRadius)
             .shadow(radius: 5)
             .overlay(
                 RoundedRectangle(cornerRadius: cornerRadius)
-                    .stroke(Color(.hotPink), lineWidth: 7)
+                    .stroke(Color(.hotPink), lineWidth: 8)
             )
             .buttonStyle(.bordered)
             .controlSize(.regular)
@@ -33,6 +33,22 @@ struct PrimeButton_: View {
 
 struct PrimeButton_Previews: PreviewProvider{
     static var previews: some View{
-        PrimeButton_(text: "Next")
+        ZStack{
+                Color(Color.black)
+                    .ignoresSafeArea()
+            Rectangle()
+                .stroke(Color.hotPink, lineWidth: 4)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    PrimeButton_(text: "Back").padding()
+                    PrimeButton_(text: "Daily Games").padding()
+                }
+                PrimeButton_(text: "Better Call Saul").padding()
+                PrimeButton_(text: "Saturday Night Live").padding()
+                
+            }
+        }
     }
 }
